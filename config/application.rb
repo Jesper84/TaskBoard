49ae30505b497104d0e0a910e55043267ddd9141
Jesper Nielsen <jesper@metallicafan.dk>
2013-04-25 14:56:03 +0200

    Precompiling jquery

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Taskboard
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #Precompile jQuery
+    config.assets.precompile += ['jquery.js']
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
